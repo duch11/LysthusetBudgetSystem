9b6e4d914704e6420fe446e07167e5727dd61a65
Jonas Ladefoged Holm <jonas@holm.tech>
2017-10-11 22:00:34 +0200

    UC 9 Kinda done

--- src/main/java/com/lysthuset/ourbudget/controller/HomeController.java
+++ src/main/java/com/lysthuset/ourbudget/controller/HomeController.java
@@ -1,21 +1,9 @@
 package com.lysthuset.ourbudget.controller;
-import com.lysthuset.ourbudget.model.entities.User;
-import com.lysthuset.ourbudget.model.repositories.IUserRepository;
-import com.lysthuset.ourbudget.model.repositories.UserArraylistRepository;
 import org.springframework.stereotype.Controller;
-
-import java.util.ArrayList;
 
 
 @Controller
 public class HomeController {
-    private ArrayList<User> usersArray = new ArrayList<>();
-    IUserRepository userRepo = new UserArraylistRepository();
 
-    /*@RequestMapping(value = {"/login"}, method = RequestMethod.POST)
-    public String login(Model model, @ModelAttribute User userToLogin){
-
-        return "hej";
-    }*/
 
 }
--- src/main/java/com/lysthuset/ourbudget/controller/UserViewController.java
+++ src/main/java/com/lysthuset/ourbudget/controller/UserViewController.java
@@ -1,15 +1,23 @@
 package com.lysthuset.ourbudget.controller;
 
+import com.lysthuset.ourbudget.model.repositories.IUserRepository;
+import com.lysthuset.ourbudget.model.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserViewController {
+    @Autowired
+    IUserRepository usersRepo = new UserRepository();
 
 
     @RequestMapping(value = "/userview", method = RequestMethod.GET)
-    public String showUserview(){
+    public String showUserview(Model model){
+        model.addAttribute("users", usersRepo.readAll());
+
         return "userview";
     }
 
